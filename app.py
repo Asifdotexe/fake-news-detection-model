deb013ac5ffca41023289865514dbb5f68f7e28b
Asif Sayyed <asifdotexe@gmail.com>
2024-06-22 14:39:49 +0530

    #5 taking title and article inputs

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 # loading the model and vectorizer
 model = joblib.load(r'..\data\fake_news_classifer.pkl')
 vectorizer = joblib.load(r'..\data\tfidf_vectorizer.pkl')
+
+# streamlit app
+st.title('Fake News Detection Application')
+
+# taking title and article inputs
+title = st.text_input('Enter the title of the news article')
+article = st.text_input('Enter the content of the news article')
