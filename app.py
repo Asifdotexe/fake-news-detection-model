8ea5b650b65f60caab5b7526e7e925e0e7a367dc
Asif Sayyed <asifdotexe@gmail.com>
2024-06-22 14:54:58 +0530

    #5 updated the path of model and vectorizer

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # loading the model and vectorizer
-model = joblib.load(r'..\data\fake_news_classifer.pkl')
-vectorizer = joblib.load(r'..\data\tfidf_vectorizer.pkl')
+model = joblib.load('fake_news_classifer.pkl')
+vectorizer = joblib.load('tfidf_vectorizer.pkl')
 
 # streamlit app
 st.title('Fake News Detection Application')
